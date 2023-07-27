487a5077b8392fbdbb89919129b42b8a7087ae35
Van <toasted.crunchy.almond@gmail.com>
2023-07-26 17:51:41 -0700

    wip: working on seconds timer

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -4,13 +4,27 @@ const Stopwatch = (props: any) => {
   const [centiSeconds, setCentiSeconds] = useState(0);
   const [isPaused, setIsPaused] = useState(true);
   const [isActive, setIsActive] = useState(false);
+  const [displayedTime, setDisplayedTime] = useState("0s 00");
+  const [_time, setTime] = useState({
+    centiseconds: 0,
+    displayedTime: "0s 00",
+  });
 
   useEffect(() => {
     let interval: any = null;
 
     if (isActive && isPaused === false) {
       interval = setInterval(() => {
-        setCentiSeconds((centiSeconds) => centiSeconds + 10);
+        setTime((t) => {
+          const newCenti = t.centiseconds + 10;
+          let secs = Math.floor(newCenti * 0.001);
+          return {
+            centiseconds: newCenti,
+            displayedTime: `${secs}s ${newCenti}`,
+          };
+        });
+        // setDisplayedTime(`${secs}s ${newCenti}`);
+        // setCentiSeconds(newCenti);
       }, 10);
     } else {
       clearInterval(interval);
@@ -31,14 +45,18 @@ const Stopwatch = (props: any) => {
 
   const handleReset = () => {
     setIsActive(false);
-    setCentiSeconds(0);
+    setTime({
+      centiseconds: 0,
+      displayedTime: "",
+    });
+    //setCentiSeconds(0);
   };
 
   return (
     <div>
       Stopwatch
       <p></p>
-      <p>{centiSeconds}</p>
+      <p>{_time.displayedTime}</p>
       <button onClick={() => handleStart()}>START</button>
       <button onClick={() => handlePauseResume()}>STOP</button>
       <button onClick={() => handleReset()}>RESET</button>
